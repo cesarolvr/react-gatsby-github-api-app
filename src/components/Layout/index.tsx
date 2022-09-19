deba43f3005c53053bf5c1e8d143abe1abd91c50
cesarolvr <cesar2012oliveira@gmail.com>
2022-09-19 18:09:27 -0300

    layout component added to wrap every route

---
+++ src/components/Layout/index.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Layout = ({ children }: React.PropsWithChildren) => {
+  return <div className="layout">{children}</div>;
+};
+
+export default Layout;
--- src/components/Search/index.tsx
+++ src/components/Search/index.tsx
@@ -1,5 +1,8 @@
-import * as React from "react";
+import React, { useContext } from "react";
 import { useForm, SubmitHandler } from "react-hook-form";
+
+// Contexts
+import { Searching } from "src/contexts";
 
 type SearchData = {
   name: string;
@@ -7,10 +10,15 @@ type SearchData = {
 
 const Search = () => {
   const { handleSubmit, register } = useForm<SearchData>();
+  const props = useContext(Searching.Context);
+
+  console.log(props);
 
   const onSubmit: SubmitHandler<SearchData> = async (data: SearchData) => {
     const { name } = data;
-    const users = await fetch(`https://api.github.com/search/users?q=${name} in:login&per_page=9&page=1`)
+    const users = await fetch(
+      `https://api.github.com/search/users?q=${name} in:login&per_page=9&page=1`,
+    )
       .then((response) => response.json())
       .then((parsedResponse) => parsedResponse);
 
