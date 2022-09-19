a3a26fcfc4f13e512f7efafa375a3e1685df849f
cesarolvr <cesar2012oliveira@gmail.com>
2022-09-19 14:12:52 -0300

    static filters working

--- src/components/Search/index.tsx
+++ src/components/Search/index.tsx
@@ -10,7 +10,7 @@ const Search = () => {
 
   const onSubmit: SubmitHandler<SearchData> = async (data: SearchData) => {
     const { name } = data;
-    const users = await fetch(`https://api.github.com/search/users?q=${name}`)
+    const users = await fetch(`https://api.github.com/search/users?q=${name} in:login&per_page=9&page=1`)
       .then((response) => response.json())
       .then((parsedResponse) => parsedResponse);
 
