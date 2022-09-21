6f2db232815ff6308145d550c4dda3e1c4a06fc1
cesarolvr <cesar2012oliveira@gmail.com>
2022-09-21 13:49:55 -0300

    snapshot test added to if component

---
+++ src/components/If/if.test.js
@@ -0,0 +1,30 @@
+import renderer from "react-test-renderer";
+
+// Component
+import If from "./index";
+
+it("render true component", () => {
+  const component = renderer.create(
+    <If
+      condition={true}
+      renderIf={<>True condition</>}
+      renderElse={<>Falsy condition</>}
+    />,
+  );
+
+  let tree = component.toJSON();
+  expect(tree).toMatchSnapshot();
+});
+
+it("render falsy component", () => {
+  const component = renderer.create(
+    <If
+      condition={false}
+      renderIf={<>True condition</>}
+      renderElse={<>Falsy condition</>}
+    />,
+  );
+
+  let tree = component.toJSON();
+  expect(tree).toMatchSnapshot();
+});
